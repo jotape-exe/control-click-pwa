2134f08682a1ee998767357c9e18c4eb2301bd28
jotape-exe <joaoxstone@gmail.com>
2023-10-08 23:22:37 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,66 @@
+<script setup lang="ts">
+import { computed, onMounted, ref } from 'vue'
+import ItemListComponent from './components/ItemListComponent.vue';
+
+const hello = ref('Hello VITE')
+const idade = ref(17)
+const userDbFake = [
+  {
+    name: 'John',
+    secondName: 'Doe',
+    phone: '+123 432 432',
+    mail: 'johndoe@email.com'
+  },
+  {
+    name: 'Alice',
+    secondName: 'Johnson',
+    phone: '+456 789 123',
+    mail: 'alice@email.com'
+  },
+  {
+    name: 'Bob',
+    secondName: 'Smith',
+    phone: '+789 321 654',
+    mail: 'bobsmith@email.com'
+  },
+  {
+    name: 'Eva',
+    secondName: 'Brown',
+    phone: '+987 654 321',
+    mail: 'eva@email.com'
+  }
+];
+
+
+onMounted(()=>{
+  hello.value = "Hello Vite & Vue"
+})
+
+const getMaiorDeIdade = computed(()=> {
+  return idade.value < 18 ? "Menor de Idade" : "Maior de Idade"
+})
+</script>
+
+<template>
+  <div style="width: 100%; display: flex; align-items: center; justify-content: center; padding: 1rem;">
+    <div style="padding: 1rem; flex-wrap: wrap; max-width: 80vw; border-radius: 10px; background-color: #7484e0; display: flex; flex-direction: row; align-items: center; justify-content: center;">
+      <h1 style="width: 100%; text-align: center; margin-bottom: 10px;">{{ hello }}</h1>
+      <br />
+      <ItemListComponent v-for="item in userDbFake"
+      :key="item"
+      :second-name="item.secondName"
+      :username="item.name"
+      :phone="item.phone"
+      :mail="item.mail"/>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.separator{
+  height: 1px;
+  width: 50%;
+  margin-top: 10px;
+  background-color: #ffffff;
+}
+</style>
---
+++ src/components/ItemListComponent.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+
+const props = defineProps(['username','secondName', 'phone', 'mail'])
+
+</script>
+
+<template>
+  <div class="card">
+      <h3>{{ props.username }}</h3>
+      <ul class="list-card">
+            <li class="list-item">{{ props.secondName }}</li>
+            <li class="list-item">{{ props.phone }}</li>
+            <li class="list-item">{{ props.mail }}</li>
+      </ul>
+      <div style="display: flex; flex-direction: row; flex-shrink: 0; align-self: flex-end;">
+          <button class="btn-1">Editar</button>
+          <button class="btn-2">Exportar</button>
+     </div>
+  </div>
+</template>
+
+<style>
+ .card{
+      display: flex;
+      flex-direction: column;
+      justify-content: space-around;
+      border-radius: 10px;
+      background-color: rgb(68, 102, 255);
+      padding: .8rem;
+      margin: 5px;
+ }
+ .list-card{
+      list-style: none;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+ }
+ .list-item{
+     margin: 0 0 5px 0;
+ }
+ button{
+     border: none;
+     border-radius: 4px;
+     padding: .4rem;
+     font-size: 20px;
+     width: 100px;
+ }
+ .btn-1{
+     background-color: #fff;
+     color: #1e31da;
+     cursor: pointer;
+     margin: 0 7px 0 0;
+ }
+
+ .btn-2{
+     color: #fff;
+     background-color: #1e31da;
+     cursor: pointer;
+ }
+</style>
